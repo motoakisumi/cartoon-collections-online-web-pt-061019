67aea910feb7bc8d346999a25c8b9e85ebb73281
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-06-27 05:00:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.any? {|word| cheese_types.include?(word)}
-    return 
+    return array.select
 end
 
